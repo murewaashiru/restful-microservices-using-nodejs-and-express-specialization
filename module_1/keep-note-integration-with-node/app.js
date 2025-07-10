66e25d785725f72cad48d5e31741d925b8c7c58f
Omomurewa George-Ashiru <rachelleashiru@gmail.com>
2025-07-10 20:41:46 +0100

    keep-note-mysql-queries cleanup

---
+++ module_1/keep-note-integration-with-node/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const config = require('./config.js');
+const dateFormat = require('date-format')
+const morgan = require('morgan')
+const dotenv = require("dotenv");
+dotenv.config();
+
+const cors = require('cors');
+var corsOptions = {
+    origin: 'http://localhost:8081',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+
+morgan.token('time',()=> dateFormat.asString(dateFormat.ISO8601_FORMAT,new Date()))
+
+app.use(morgan('[:time] :remote-addr :method :url :status :res[content-length] :response-time ms'));
+
+//Register routes
+const authRoutes = require('./src/routes/auth.routes.js');
+const indexRoutes = require('./src/routes/index.routes.js');
+const userRoutes = require('./src/routes/user.routes.js');
+app.use('/api/auth', authRoutes);
+app.use('/api', indexRoutes);
+app.use('/api/users', userRoutes);
+
+
+//const PORT = process.env.PORT || 8081;
+app.listen(config.PORT, ()=>{
+    console.log(`Server is running on port ${config.PORT}.`);
+});
+
+// Export the app for testing purposes
+module.exports = app;
--- module_1/keep-note-integration-with-node/config.js
+++ module_1/keep-note-integration-with-node/config.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const config ={
     PORT: process.env.PORT ||3000,
     AUTH_SECRET: process.env.AUTH_SECRET || "secret",
     DATABASE_HOST: process.env.DATABASE_HOST || "localhost",
     DATABASE_USER: process.env.DATABASE_USER || "root",
-    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || "Password1!",
-    DATABASE_NAME: process.env.DATABASE_NAME || "polyglot_persistence",
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || "Password",
+    DATABASE_NAME: process.env.DATABASE_NAME || "notesdb",
 }
 
 module.exports = config
